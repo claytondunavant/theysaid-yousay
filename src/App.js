a7904eebaa40ff19b320d58b3f4e8be3e048f906
clayton <clayton@claytondunavant.com>
2021-06-02 15:32:55 -0500

    most basic theysaid-yousay

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './css/App.css'
+
+const App = () => {
+  const [theyString, setTheyString] = useState("Hope you are having a good day")
+  const [youString, setYouString] = useState("")
+  
+  const changeInputHandler = (event) => {
+    setYouString(event.target.value)
+  }
+  
+  const sumbitHandler = (event) => {
+    //prevent page reloading
+    event.preventDefault()
+    
+    //set theyString to youString
+    setTheyString(youString)
+
+    //set youString to nothing
+    setYouString("")
+    
+    //close window?
+  }
+
+  return (
+    <div className="center">
+      <h1>They Said:</h1>
+      <h2>{theyString}</h2>
+
+      <h1>You Say:</h1>
+
+      <form onSubmit={sumbitHandler}>
+        <input
+          value={youString}
+          onChange={changeInputHandler}
+        >
+        </input>
+        <button type="submit">tell them</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default App;
